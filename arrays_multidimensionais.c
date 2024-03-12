b7d95830a16bc9855961d1b9d84a90ccaa2fbce5
BeckRonaldy <ronaldybeck@gmail.com.br>
2024-03-12 14:44:38 -0400

    atualizações de código

---
+++ arrays_multidimensionais.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* Matriz bi-dimenssional
+* [00][01][02]
+* [10][11][12]
+* [20][21][22]
+*
+* int -> 4 bytes
+* linha-3 x coluna-3 x int-4 = 36 bytes
+*
+*
+*/
+
+int main(){
+    int *p, linhas=3, colunas=3;
+
+    p = (int*)malloc(linhas * colunas * sizeof(int));
+
+    for(int i=0; i<linhas; i++){
+        for(int j=0; j<colunas; j++){
+            p[i * colunas + j] = 3 * i + j;
+        }
+    }
+
+    for(int i=0; i<linhas; i++){
+        printf("\n");
+        for(int j=0; j<colunas; j++){
+            printf("[%d] ", p[i * colunas + j]);
+        }
+    }
+
+    free(p);
+    p = NULL;
+
+    return 0;
+}
